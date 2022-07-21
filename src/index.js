7bcb92923b3ea807a016f9434dfa3940f2159c3d
amrelkoumy <amr.m.elkoumy@gmail.com>
2022-07-21 12:51:38 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import hello from './hello world';
+import header from "./component/header/header"
+import "./style.css"
+import bio from "./component/bio/bio"
+import photo from"./assets/cover.jpg"
+
+// hello()
